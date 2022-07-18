29dbf64c697fe4de891017baa1a806f8b0aa12f5
MUHAMMED IRSHAD <irshadaj08@gmail.com>
2022-07-19 00:39:55 +0530

    Tic Tac Toe Game without using pointers

---
+++ tic-tac-toe/tic-tac-toe.c
@@ -0,0 +1,214 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<ctype.h>
+#include<time.h>  //needed header files
+
+
+char board[3][3];            //3x3 board for game
+const char PLAYER = 'X';        //player sign X
+const char COMPUTER = 'O';      //computer sign O
+
+
+void resetBoard();      //To reset the board
+void printBoard();      //To print the board
+int checkFreespace();   //checking free space
+void playerMove();      //define player move
+void computerMove();    //define computer movement
+char checkWinner();     // checking the winner
+void printWinner(char); //printing the winner taking a char argument
+
+
+
+int main(){
+
+    char winner = ' ';
+    char response;
+
+    do{
+
+    winner = ' ';
+    response=' ';
+    resetBoard();
+
+    while(winner=' ' && checkFreespace()!= 0){
+
+        printBoard();
+
+        playerMove();
+        winner = checkWinner();
+
+        if(winner != ' ' || checkFreespace ==0){
+
+            break;
+        }
+
+        computerMove();
+        winner = checkWinner();
+
+        if(winner != ' ' || checkFreespace ==0){
+
+            break;
+        }
+
+
+    }printBoard();
+    printWinner(winner);
+
+    printf("\n would you like you to play again:\n");
+    scanf(" %c");
+    scanf(" %c",&response);
+    response=toupper(response);
+
+    }while(response=='Y');
+
+printf("Thanks for playing");
+
+
+return 0;
+}
+
+
+void resetBoard(){
+
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            board[i][j]=' ';
+
+        }
+
+    }
+
+
+
+}
+void printBoard(){
+
+printf(" %c | %c | %c ",board[0][0],board[0][1],board[0][2]);
+printf("\n---|---|----\n");
+printf(" %c | %c | %c ",board[1][0],board[1][1],board[1][2]);
+printf("\n---|---|----\n");
+printf(" %c | %c | %c ",board[2][0],board[2][1],board[2][2]);
+//printf("\n---|---|----\n");
+printf("\n\n");
+
+
+
+
+
+
+
+}
+int checkFreespace(){
+
+int freeSpace = 9;
+ for(int i=0;i<3;i++){
+
+    for(int j=0;j<3;j++){
+
+        if(board[i][j] != ' '){
+            freeSpace--;
+        }
+    }
+ }
+
+ return freeSpace;
+
+}
+
+
+void playerMove()
+{
+    int x,y;
+    do{
+
+    printf("Enter row number (1-3): ");
+    scanf(" %d",&x);
+    x--;
+
+    printf("\nEnter column number (1-3): ");
+    scanf(" %d",&y);
+    y--;
+
+    if(board[x][y]!=' '){
+        printf("Invalid\n");
+
+
+    }else{
+
+    board[x][y]=PLAYER;
+    break;
+    }
+    }while(board[x][y]!=' ');
+
+
+}
+
+void computerMove(){
+    srand(time(0)); //create a seed based on current time
+    int x,y;
+
+    if(checkFreespace()>0){
+        do{
+        x=rand()%3;
+        y=rand()%3;
+
+    }while(board[x][y]!=' ');
+
+    board[x][y]=COMPUTER;
+
+    }else{
+
+    printWinner(' ');
+
+    }
+
+
+}
+
+char checkWinner(){
+
+//check for rows
+for(int i=0;i<3;i++){
+
+    if(board[i][0]==board[i][1]&&board[i][0]==board[i][2])
+    {
+
+        return board[i][0];
+    }
+}
+//check for columns
+for(int i=0;i<3;i++){
+
+    if(board[0][i]==board[1][i]&&board[0][i]==board[2][i])
+    {
+
+        return board[0][i];
+    }
+}
+ //check diagonals
+ if(board[0][0]==board[1][1]&&board[0][0]==board[2][2])
+    {
+
+        return board[0][0];
+    }
+
+    if(board[0][2]==board[1][1]&&board[0][2]==board[2][1])
+    {
+
+        return board[0][2];
+    }
+
+    return ' ';
+}
+void printWinner(char winner){
+
+if(winner==PLAYER){
+    printf("\nYou win");
+}else if(winner==COMPUTER){
+    printf("\nComputer wins");
+}else{
+    printf("\nDraw\n");
+}
+}
